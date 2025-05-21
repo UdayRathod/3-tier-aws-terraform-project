8787722cb7a102c089c753c3b1eb9211d1ec4f6b
unknown <rathoduday1998@gmail.com>
2025-05-21 18:39:31 +0530

    updated confg for s3 & backend

--- s3.tf
+++ s3.tf
@@ -16,3 +16,29 @@ resource "aws_s3_object" "website_content" {
   tags = local.common_tags
 
 }
+
+
+
+# S3 bucket for backend, we did not used aboe s3 module as that module is create iam polices, role releated to ALB.
+
+resource "aws_s3_bucket" "backend-s3" {
+  bucket = "s3-bucket-terraform-terracloud-state"
+
+  tags = local.common_tags
+}
+
+# Enabling default encryption on backend S3 bucket
+resource "apply_server_side_encryption_by_default" "backend-s3-encrpytion" {
+  bucket = aws_s3_bucket.backend-s3.id
+  rule {
+    sse_algorithm = "AES256"
+  }
+}
+
+# Enabling versioning on backend S3 bucket
+resource "aws_s3_bucket_versioning" "backend-s3-versioning" {
+  bucket = aws_s3_bucket.backend-s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
